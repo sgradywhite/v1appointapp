e1c827c1ca9e6fc5b051bd9c83593a3ca82834d9
Stephan White <sgradywhite@yahoo.com>
2016-11-10 11:22:45 +0000

    changes the host name in development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mysterious-peak-30987.herokuapp.com'
+  host = 'protected-oasis-72933.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
