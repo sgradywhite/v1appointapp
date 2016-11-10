170d1a76d35cd1621634c0931cc13751ed112a66
Stephan White <sgradywhite@yahoo.com>
2016-11-10 11:44:37 +0000

    add title, html and body to index for office

--- config/routes.rb
+++ config/routes.rb
@@ -97,7 +97,7 @@ Rails.application.routes.draw do
     post   'login'   => 'sessions#create'
     delete 'logout'  => 'sessions#destroy'
     
-    get   'appointments' => 'appointments#index'
+    get   'appointment' => 'appointments#index'
     get   'edit_appointment' => 'appointments#edit'
     get   'new_appointment'  => 'appointments#new'
     
